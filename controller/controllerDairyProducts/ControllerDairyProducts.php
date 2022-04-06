5cc11abc04395ea2f58257a0366c7e7d73469db9
Couturier alexandre <couturier@MacBook-Pro-de-Couturier.local>
2022-04-06 15:27:31 +0200

    create folder controller/ router and class dairyproducts

---
+++ controller/controllerDairyProducts/ControllerDairyProducts.php
@@ -0,0 +1,52 @@
+<?php
+echo "dans le controller DairyProducts avant la class";
+include($_SERVER['DOCUMENT_ROOT']."/BAndD/model/DairyProducts.php");
+Class ControllerDairyProducts{
+
+
+    public static function getAllDairyProducts(){
+        $allRows = DairyProducts::getAllDairyProducts();
+        include($_SERVER['DOCUMENT_ROOT']."/BAndD/view/test.php");
+    }
+
+    public static function getDairyProductsById($idDairyProducts){
+        $row = DairyProducts::getDairyProductsByid($idDairyProducts);
+        include($_SERVER['DOCUMENT_ROOT']."/BAndD/view/viewDairyProducts/viewDairyProductsById.php");
+
+    }
+
+    public static function createDairyProducts($post){
+        echo "dans la fonction createDairyProducts dans le controller";
+        var_dump($post);
+        $DairyProducts = new DairyProducts($post['productName'], $post['animalOrPlant'], $post['state'], $post['fermentation']);
+
+        $DairyProducts->createDairyProducts();
+        self::getAllDairyProducts();
+    }
+
+   public static function updateDairyProducts($post){
+        $DairyProducts = new DairyProducts($post['productName'], $post['animalOrPlant'], $post['state'],$post['fermentation'],$post['idDairyProducts']);
+
+        $DairyProducts->updateDairyProducts();
+        self::getAllDairyProducts();
+    }
+
+    public static function deleteDairyProductsById($id){
+       
+        DairyProducts::deleteDairyProductsById($id);
+        self::getAllDairyProducts();
+    }
+
+    public static function formDairyProductsUpdate($get){
+
+        $row = DairyProducts::getDairyProductsByid($get);
+        include($_SERVER['DOCUMENT_ROOT']."/BAndD/view/viewDairyProducts/formDairyProductsUpdate.php");
+
+    }
+
+    public static function formOffer(){
+
+        require "./View";
+    }
+
+}
---
+++ model/DairyProducts.php
@@ -0,0 +1,82 @@
+<?php 
+echo "dans la class dairyProducts";
+require_once "DAO.php";
+
+class DairyProducts {
+    private $idDairyProducts;
+    private $productName;
+    private $animalOrPlant;
+    private $state;
+    private $fermentation;
+
+    public function __construct($productName, $animalOrPlant, $state, $fermentation, $idDairyProducts = NULL){
+        $this->productName = $productName;
+        $this->animalOrPlant = $animalOrPlant;
+        $this->state = $state;
+        $this->fermentation = $fermentation;
+        $this->idDairyProducts = $idDairyProducts;
+    }
+
+    public function createDairyProducts(){
+        echo "dans la fonction createDrink";
+        $dao = new DAO();
+        $dbh = $dao->getDbh();
+       
+       $stmt = $dbh->prepare("INSERT INTO dairyProducts (productName, animalOrPlant, state, fermentation) VALUES (:un, :deux, :trois, :quatre);");
+       
+       $stmt->bindParam(':un', $this->productName);
+        $stmt->bindParam(':deux', $this->animalOrPlant);
+        $stmt->bindParam(':trois', $this->state);
+        $stmt->bindParam(':trois', $this->fermentation);
+        $stmt->execute();
+    }
+
+    public static function getAllDairyProducts(){
+        $dao = new DAO();
+        $dbh = $dao->getDbh();
+        $stmt = $dbh->prepare("SELECT * FROM dairyProducts;");
+        $stmt->execute();
+        $allRows = $stmt->fetchAll();
+        
+        return $allRows;
+    }
+
+        public static function getDairyProductsById($idDairyProducts){
+        $dao = new DAO();
+        $dbh = $dao->getDbh();
+        
+        $stmt = $dbh->prepare("SELECT * FROM dairyProducts WHERE idDairyProducts=:iddairyproducts;");
+        $stmt->bindParam(":iddairyproducts", $idDairyProducts);
+        
+        $stmt->execute();
+
+        return $stmt->fetch();
+    }
+
+    public function updateDairyProducts(){
+        $dao = new DAO();
+        $dbh =$dao->getDbh();
+
+        $stmt = $dbh->prepare("UPDATE dairyProducts SET productName=:add, animalOrPlant=:zip, state=:city,fermentation=:quatre  WHERE idDairyProducts=:idDairyProducts");
+
+        $stmt->bindParam(':add', $this->productName);
+        $stmt->bindParam(':zip',$this->animalOrPlant);
+        $stmt->bindParam(':idDairyProducts',$this->idDairyProducts);
+        $stmt->bindParam(':city',$this->state);
+        $stmt->bindParam(':quatre',$this->fermentation);
+        
+      return $stmt->execute();
+    }
+
+    public static function deleteDairyProductsById($idDairyProducts){
+        $dao = new DAO();
+        $dbh = $dao->getDbh();
+        $stmt = $dbh->prepare("DELETE FROM drink WHERE idDairyProducts = :idDairyProducts;");
+         $stmt->bindParam("idDairyProducts",$idDairyProducts);
+         $stmt->execute();
+ 
+        
+         
+    }
+
+}
