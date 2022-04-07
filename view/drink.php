53570373676bc6ed99010671e567cc37c1a1b81a
TTHELLIER <t.thellier@gmail.com>
2022-04-07 15:36:16 +0200

    drink.php completé CSS en cours

---
+++ view/drink.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/style.css">
+    <title>Les Boissons</title>
+</head>
+<body>
+    <?php
+        require_once "../model/Header.php"
+    ?>
+
+    <div id=main>
+        <h2>Les Boissons</h2>
+
+        <div id=drink></div>
+
+        <h4>Eau plate/Eau Gazeuse</h4>
+
+        <h5>Rôle:</h5>
+        <p>Permet de compenser quotidiennement les pertes urinaires (1 500 ml/j) et fécales (100-150 g/j), ou par voie cutanée (pertes sudorales liées aux besoins de la thermorégulation très variable selon les conditions) et par voie respiratoire (800 ml/j).</p>
+    
+        <div id=otherdrink>
+            <div id=tea></div>
+            <div id=coffe></div>
+        </div>
+
+            <h5>Besoins</h5>
+            <p>1,2 L à 2,5 L au minimum selon l’activité physique et les conditions climatiques.
+        Il est conseillé de varier les eaux : du robinet ou en bouteilles (minérales, de source) puisque chacune d’entre elles est plus ou moins composée en minéraux.</p>
+
+</div>
+</body>
+</html>
