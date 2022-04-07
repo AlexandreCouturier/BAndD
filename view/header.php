c4b54f2131039fc3b957b6d093890bd80d658ed3
sylvainflt <sylvainfoucault1@gmail.com>
2022-04-07 10:39:59 +0200

    commencement de la page d'accueil

---
+++ view/header.php
@@ -0,0 +1,15 @@
+
+<header>
+  <img src="./image/logo.svg" alt="logo" id="logo" />
+  <nav>
+    <ul class="nav">
+      <a href=""><li>Accueil</li></a>
+      <a href=""><li>Associations</li></a>
+      <a href=""><li>Ajouter produit</li></a>
+      <a href=""><li>Catégorie</li></a>
+    </ul>
+  </nav>
+</header>
+<div class="degradeVert"></div>
+
+ 
---
+++ view/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>BandD</title>
+  <link rel="stylesheet" href="./style/style.css">
+</head>
+
+<body>
+
+  <?php
+  include 'header.php';
+  ?>
+
+  <main>
+    <h1>Les grandes règles des bonnes associations alimentaires</h1>
+    <section class="digestion">
+      <article>
+        <h3>Pourquoi faire attention aux associations alimentaires?</h3>
+        <p>Hippocrate affirmait qu’il fallait éviter de mélanger les <span class="texteOrange">aliments</span> qui se font la
+          guerre dans le tube digestif car certains aliments n’ont pas le même
+          <span class="texteOrange">temps de digestion</span>, ni le même lieu. S’ils sont consommés ensemble et
+          selon sa sensibilité intestinale, ils peuvent créer des problèmes
+          digestifs tels que des ballonnements, des lourdeurs, des gaz, des
+          intolérances alimentaires. Ainsi un <span class="texteOrange">repas complet</span> comprenant des
+          protéines, des féculents, du gras et du sucre ne fait pas bon ménage
+          et nous passons des heures à le digérer, ce qui <span class="texteOrange">fatigue</span> notre
+          organisme.
+        </p>
+      </article>
+      <article>
+        <h3>Comprendre la digestion des macronutriments</h3>
+        <p>Voyons ensemble la <span class="texteOrange">composition</span> de nos principaux aliments (glucides,
+          protéines et lipides) et le <span class="texteOrange">fonctionnement</span> de nos organes digestifs pour
+          mieux comprendre les bonnes associations alimentaires.
+        </p>
+      </article>
+
+      <p>Le <span class="gras">SYSTÈME DIGESTIF</span> est un tube où se succède différents organes:
+      </p>
+
+      <aside>
+        <figure>
+          <img src="" alt="">
+        </figure>
+      </aside>  
+      <article>
+        
+      </article>
+
+
+
+    </section>
+  </main>
+
+</body>
+
+</html>
