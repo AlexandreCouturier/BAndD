ef69be93d930634578768c02ca51da623ebdbe43
Alain <alainczr22@gmail.com>
2022-04-07 14:03:38 +0200

    create folder read by category

--- view/searchByCategory.php
+++ view/searchByCategory.php
@@ -8,14 +8,17 @@
 </head>
 <body>
 
-      <select action="../router/RouterFruitAndVegetable.php" name="" id="">
-            <option value=""><a href="./formOilyMaterial.php">Ajouter une matière grasse</a></option>
-            <option value=""><a href=""></a></option>
-            <option value=""><a href=""></a></option>
-            <option value=""><a href=""></a></option>
-            <option value=""><a href=""></a></option>
-      </select>
-       
 
+    <form name="form" >
+        
+        <select size="1"  onChange="location = this.options[this.selectedIndex].value;">
+	        <option value="lien.html" >1</option>
+	        <option value="lien.html" >2</option>
+	        <option value="lien.html" >3</option>
+        </select>
+
+    </form>
+       
+<script ></script>
 </body>
 </html>
