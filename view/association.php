68d459204e5f4accbe1647527ab01060c3a1feaf
sylvainflt <sylvainfoucault1@gmail.com>
2022-04-07 15:23:32 +0200

    2e page, les bonnes et mauvaises associations

---
+++ view/association.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BandD associations</title>
+    <link rel="stylesheet" href="./style/style.css">
+</head>
+
+<body>
+
+    <?php
+    include 'header.php';
+    ?>
+
+    <main>
+
+        <h1>Les bonnes associations</h1>
+        <section class="bonnesAssociations">
+            <div class="troisParties">
+                <aside>
+                    <figure class="gauche">
+                        <img id="fillette" src="./image/fillette.png" alt="fillette">
+                    </figure>
+                </aside>
+                <div>
+                    <article>
+                        <h3>1/ Fer + vitamine C </h3>
+                        <p>
+                            Le fer est présent sous deux formes:
+                        </p>
+                        <p>
+                            – le fer <span class="texteOrange">héminique</span> que l’on trouve dans les produits d’origine <span class="texteOrange">animale</span> viandes, poissons,
+                            œufs,… Il est très bien absorbé par la muqueuse intestinale et représente 70% du fer
+                            total.
+                        <p>
+                            – le fer <span class="texteOrange">non héminique</span> que l’on trouve dans les <span class="texteOrange">végétaux</span> légumes, céréales,
+                            légumineuses, germe de blé, oléagineux. Il est beaucoup moins bien absorbé par la
+                            muqueuse intestinale, à peine 30% du fer total.</p>
+                        <p>
+                            Son absorption est meilleure lorsque le fer est accompagné de vitamine C présente dans
+                            les légumes et les fruits. Pour optimiser ses apports en fer, surtout dans l’alimentation
+                            végétarienne, il faudra donc veiller à associer au cours d’un même repas de la
+                            <span class="texteOrange">vitamine C</span> (agrumes, persil, brocolis, poivrons, épinards…) aux aliments qui
+                            sont source de fer non héminique.
+                        </p>
+                        <p>
+                            Pour améliorer la <span class="texteOrange">biodisponibilité</span> du fer non héminique, et favoriser sa bonne <span class="texteOrange">assimilation</span>,
+                            il suffit donc d’associer au cours du même repas des aliments riches en vitamines C: jus de
+                            citrons, légumes, fruits…</p>
+
+                        </p>
+                    </article>
+                    <article>
+                        <h3>2/ Vitamine C + sélénium</h3>
+                        <p>
+                            Le sélénium est un <span class="texteOrange">oligo-élément</span> qui détruit les radicaux libres (des molécules chimiques
+                            instables produites en faible quantité mais qui, en surnombre, ont un impact sur le
+                            vieillissement tissulaire ou accélèrent certaines maladies). On trouve le sélénium dans de
+                            nombreux <span class="texteOrange">végétaux</span>, dans les <span class="texteOrange">fruits secs</span>, les <span class="texteOrange">œufs</span> et surtout dans les <span class="texteOrange">poissons</span> et fruits de
+                            mer. Une carence en sélénium entraîne par exemple une accélération du
+                            vieillissement.</p>
+                        <p>
+                            La vitamine C contenue dans les fruits et les légumes augmente la
+                            biodisponibilité du sélénium contenu dans ces sources alimentaires. Pour
+                            optimiser son <span class="texteOrange">absorption</span>, on veillera donc à les associer à des aliments contenant de la
+                            vitamine C.
+
+                        </p>
+                    </article>
+                    <article>
+                        <h3>3/ Légumineuses + céréales
+                        </h3>
+                        <p>
+                            L’organisme a besoin de <span class="texteOrange">8 acides aminés</span> essentiels pour un apport complet de protéines.
+                            Ils sont dits « <span class="texteOrange">essentiels</span> » car apportés uniquement par l’alimentation, le corps ne sachant
+                            pas les fabriquer. Les protéines végétales doivent être associées entre elles pour que les
+                            acides aminés puissent se compléter.
+                            Dans les céréales, il manque un acide aminé: la <span class="texteOrange">lysine</span>. Et dans les légumineuses, il
+                            manque un acide aminé: la <span class="texteOrange">méthionine</span>.
+                            Il faut donc associer les céréales (pain, pâtes, riz, semoule,…) aux légumineuses (haricots
+                            secs, fèves, pois chiches, lentilles,…) ou à des <span class="texteOrange">oléagineux</span> (noix, noisettes, noix de cajou,
+                            amandes,…) pour bénéficier des 8 acides aminés essentiels.
+
+                            Exemple : haricots secs et riz, pois chiche et semoule, lentilles et riz, haricots secs et
+                            pâtes, riz et noix de cajou.
+
+                        </p>
+                    </article>
+                    <article>
+                        <h3>4/ Association féculents + légumes
+                        </h3>
+                        <p>
+                            La vitesse de <span class="texteOrange">digestion</span> des glucides définie par l’index glycémique va dépendre de la
+                            manière dont vous allez les consommer.</p>
+                        <p>
+                            Si vous consommez des pommes de terre seules au cours de votre repas, votre taux de
+                            <span class="texteOrange">glycémie</span> va augmenter de façon brutale dans le sang, de la même manière que si vous
+                            consommiez une baguette de pain blanc ou un jus de fruit. Le sucre de ces aliments est
+                            <span class="texteOrange">rapidement assimilé</span> ce qui va entraîner la stimulation d’<span class="texteOrange">insuline</span>, l’hormone de stockage
+                            des graisses, et donc une prise de poids.
+                        </p>
+                        <p>
+                            Pour diminuer l’index glycémique d’un repas contenant des féculents et/ou des
+                            sucres rapides, il est recommandé d’associer au cours de ce même repas des
+                            <span class="texteOrange">féculents</span> et/ou sucres rapides avec des aliments riches en <span class="texteOrange">fibres</span> (légumes, fruits).
+                            Car en effet, les glucides quelle que soit leur origine sont assimilés différemment selon
+                            leur consommation.
+                        </p>
+                        <p>
+                            Exemple : Pâtes et légumes, Riz et légumes, Pommes de terre et légumes.
+                            Si, par exemple au petit-déjeuner, vous ne pouvez vous passer de vos céréales sucrées
+                            (index glycémique rapide), ajoutez-y un fruit frais et surtout pas un jus de fruit (index
+                            glycémique rapide) pour ralentir l’absorption du sucre contenu à ce repas.
+                            En les <span class="texteOrange">associant</span> ainsi, vous serez <span class="texteOrange">rassasiés</span> plus longtemps et éviterez une prise de poids.
+                        </p>
+                    </article>
+                    <article>
+                        <h3>5/ Glucides complexes et lipides</h3>
+                        <p>
+                            L’association glucides et matières grasses va ralentir l’absorption des glucides qui seront
+                            digérés plus <span class="texteOrange">lentement</span>, ce qui va permettre d’approvisionner l’organisme sans à-coups et
+                            réduire l’index glycémique du repas.
+                        </p>
+                        <p>
+                            Exemple : Pain et beurre ou purée d’oléagineux (en quantité raisonnable), pâtes et filet
+                            d’huile d’olive.
+                        </p>
+                    </article>
+
+                </div>
+                <aside>
+                    <figure class="droite">
+                        <img id="garçon" src="./image/garÃ§on.png" alt="garçon">
+
+                    </figure>
+                </aside>
+            </div>
+        </section>
+        <h2>Les mauvaises associations</h2>
+        <section class="mauvaisesAssociations">
+            <div class="troisPartiesSuite">
+                <div class="imageGauche"></div>
+                <div>
+                    <article>
+                        <h3>1/ Fer + café ou thé</h3>
+                        <p>
+                            Il faut savoir qu’une consommation <span class="texteOrange">excessive</span> de thé, de café ou de calcium entraîne une
+                            diminution de l’absorption du fer. Pour limiter les pertes en fer, il faudra veiller à
+                            consommer ces boissons <span class="texteOrange">en dehors des repas</span> et/ou à y ajouter des aliments riches en
+                            vitamine C pour neutraliser les méfaits des tanins.
+                        </p>
+                    </article>
+                    <article>
+                        <h3>2/ Protéines animales et féculents
+                        </h3>
+                        <p>
+                            Il faut éviter leur association seule qui provoque des troubles digestifs souvent
+                            désagréables.</p>
+                        <p>
+                            Pour limiter ces désagréments, l’apport de <span class="texteOrange">fibres</span> sous forme de légumes diminuera
+                                les troubles digestifs provoqués par l’association de ces deux catégories
+                                d’aliments.
+                        </p>
+                    </article>
+                </div>
+                <aside>
+                    <figure>
+                        <img id="malAuVentre" src="./image/malAuVentre.png" alt="mal Au Ventre">
+                    </figure>
+                </aside>
+            </div>
+        </section>
+
+    </main>
+
+</body>
+
+</html>
