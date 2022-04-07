78e2908c0e6c577633ab5bffb715101da311c13f
TTHELLIER <t.thellier@gmail.com>
2022-04-07 14:44:09 +0200

    allCategories OK sans footer

---
+++ model/Header.php
@@ -0,0 +1,13 @@
+
+<header>
+  <img src="../view/images/logo.svg" alt="logo" id="logo" />
+  <nav>
+    <ul class="nav">
+      <a href=""><li>Accueil</li></a>
+      <a href=""><li>Associations</li></a>
+      <a href=""><li>Ajouter produit</li></a>
+      <a href=""><li>Catégorie</li></a>
+    </ul>
+  </nav>
+</header>
+<div class="degradeVert"></div>
