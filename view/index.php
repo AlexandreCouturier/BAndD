57000ebbc70ccedb737ec9496d72557a6a2b35d4
sylvainflt <sylvainfoucault1@gmail.com>
2022-04-07 11:44:05 +0200

    page accueil à moitié

--- view/index.php
+++ view/index.php
@@ -18,41 +18,128 @@
   <main>
     <h1>Les grandes règles des bonnes associations alimentaires</h1>
     <section class="digestion">
-      <article>
-        <h3>Pourquoi faire attention aux associations alimentaires?</h3>
-        <p>Hippocrate affirmait qu’il fallait éviter de mélanger les <span class="texteOrange">aliments</span> qui se font la
-          guerre dans le tube digestif car certains aliments n’ont pas le même
-          <span class="texteOrange">temps de digestion</span>, ni le même lieu. S’ils sont consommés ensemble et
-          selon sa sensibilité intestinale, ils peuvent créer des problèmes
-          digestifs tels que des ballonnements, des lourdeurs, des gaz, des
-          intolérances alimentaires. Ainsi un <span class="texteOrange">repas complet</span> comprenant des
-          protéines, des féculents, du gras et du sucre ne fait pas bon ménage
-          et nous passons des heures à le digérer, ce qui <span class="texteOrange">fatigue</span> notre
-          organisme.
+      <div class="systemeDigestif">
+        <article>
+          <h3>Pourquoi faire attention aux associations alimentaires?</h3>
+          <p>Hippocrate affirmait qu’il fallait éviter de mélanger les <span class="texteOrange">aliments</span> qui se font la
+            guerre dans le tube digestif car certains aliments n’ont pas le même
+            <span class="texteOrange">temps de digestion</span>, ni le même lieu. S’ils sont consommés ensemble et
+            selon sa sensibilité intestinale, ils peuvent créer des problèmes
+            digestifs tels que des ballonnements, des lourdeurs, des gaz, des
+            intolérances alimentaires. Ainsi un <span class="texteOrange">repas complet</span> comprenant des
+            protéines, des féculents, du gras et du sucre ne fait pas bon ménage
+            et nous passons des heures à le digérer, ce qui <span class="texteOrange">fatigue</span> notre
+            organisme.
+          </p>
+        </article>
+        <article>
+          <h3>Comprendre la digestion des macronutriments</h3>
+          <p>Voyons ensemble la <span class="texteOrange">composition</span> de nos principaux aliments (glucides,
+            protéines et lipides) et le <span class="texteOrange">fonctionnement</span> de nos organes digestifs pour
+            mieux comprendre les bonnes associations alimentaires.
+          </p>
+        </article>
+
+        <p>Le <span class="gras">SYSTÈME DIGESTIF</span> est un tube où se succède différents organes:
         </p>
-      </article>
-      <article>
-        <h3>Comprendre la digestion des macronutriments</h3>
-        <p>Voyons ensemble la <span class="texteOrange">composition</span> de nos principaux aliments (glucides,
-          protéines et lipides) et le <span class="texteOrange">fonctionnement</span> de nos organes digestifs pour
-          mieux comprendre les bonnes associations alimentaires.
-        </p>
-      </article>
-
-      <p>Le <span class="gras">SYSTÈME DIGESTIF</span> est un tube où se succède différents organes:
-      </p>
-
-      <aside>
-        <figure>
-          <img src="" alt="">
-        </figure>
-      </aside>  
-      <article>
-        
-      </article>
-
-
-
+        <section class="bouche">
+          <aside>
+            <figure class="gauche">
+              <img id="dent" src="./image/dent.png" alt="une dent">
+            </figure>
+          </aside>
+          <article>
+            <p>
+              la <span class="texteVert gras">BOUCHE</span> contient les dents, qui ont pour but de réduire nos
+              aliments en « <span class="texteOrange">bouillie</span> » (appelé bol alimentaire) pour permettre le
+              passage de ce bol vers l’estomac via l’œsophage.
+              C’est dans la <span class="texteOrange">bouche</span> que commence la digestion des glucides (pain,
+              pâtes, riz, céréales, pomme de terre…) grâce à la <span class="texteOrange">salive</span> qui contient des
+              enzymes digestives spécifiques pour ces amidons. C’est également le
+              milieu alcalin de la bouche qui facilite cette digestion.
+              Vous l’aurez bien compris, il est primordial de pendre le temps de bien
+              <span class="texteOrange">mastiquer</span> et de saliver pour préparer la digestion des aliments (surtout
+              des glucides).
+            </p>
+          </article>
+        </section>
+        <section class="estomac">
+          <article>
+            <p>
+              l’<span class="texteVert gras">ESTOMAC</span> est un organe contenant un pH très acide grâce à
+              l’activité de l’<span class="texteOrange">acide chlorhydrique</span> et du <span class="texteOrange">suc gastrique</span>. Il est le lieu idéal
+              pour la digestion des protéines.
+            </p>
+          </article>
+          <aside>
+            <figure class="droite">
+              <img id="estomac" src="./image/estomac.png" alt="l'estomac">
+            </figure>
+          </aside>
+        </section>
+        <section class="intestin">
+          <aside>
+            <figure class="gauche">
+              <img id="intestin" src="./image/intestin.png" alt="l'intestin">
+            </figure>
+          </aside>
+          <article>
+            <p>
+              l’<span class="texteVert gras">INTESTIN GRELE</span> et plus précisément sa première partie, appelée
+              duodénum est le <span class="texteOrange">carrefour des digestions</span>. A l’aide du pancréas, celui-ci
+              déverse dans le duodénum, un suc pancréatique contenant les enzymes
+              capables de <span class="texteOrange">dégrader</span> les trois types d’aliments et de poursuivre le
+              travail déjà effectué en amont dans la bouche (glucides) et l’estomac
+              (protéines). Et le lipides dans tout ça??? Ces derniers vont également
+              subir un processus de transformation grâce à la <span class="texteOrange">bile</span> (stockée dans la
+              vésicule biliaire et produite par la foie), qui va les réduire en fines
+              <span class="texteOrange">gouttelettes</span>. Vient ensuite le processus d’<span class="texteOrange">assimilation</span>, c’est-à-dire le
+              passage des 3 types nutriments vers l’intérieur de l’organisme, qui aura
+              principalement lieu dans la seconde partie de l’intestin grêle (jéjunumiléon).
+            </p>
+          </article>
+        </section>
+        <section class="foie">
+          <article>
+            <p>
+              Le <span class="texteVert gras">FOIE</span>, est un organe annexe du tube digestif mais joue un rôle très
+              important car les <span class="texteOrange">nutriments absorbés</span> y sont acheminés, stockés voire
+              transformés pour certains, puis répartis en fonction des besoins. Le foie
+              gère également les <span class="texteOrange">déchets</span> (de l’alimentation, des médicaments, des
+              produits chimiques comme les pesticides,…). C’est l’organe de la <span class="texteOrange">détox</span>
+              par excellence !!!
+            </p>
+          </article>
+          <aside>
+            <figure class="droite">
+              <img id="foie" src="./image/foie.png" alt="le foie">
+            </figure>
+          </aside>
+        </section>
+        <section class="colon">
+          <aside>
+            <figure class="gauche">
+              <img id="colon" src="./image/colon.png" alt="le colon">
+            </figure>
+          </aside>
+          <article>
+            <p>
+              le <span class="texteVert gras">COLON</span> ou le <span class="texteOrange">gros intestin</span> est la dernière partie du système digestif.
+              Il s’occupe principalement de l’<span class="texteOrange">élimination</span> des déchets et des matières
+              alimentaires non digérés. Les matières résiduelles sont enfin éliminées
+              par la voie rectale.
+            </p>
+          </article>
+        </section>
+        <article>
+          <p>
+            Le rôle du <span class="texteVert">système digestif</span> est avant tout de transformer nos aliments en
+            <span class="texteOrange">nutriments</span> pour qu’ils soient assimilés par l’organisme. Une digestion
+            perturbée peut ainsi entraver cette assimilation et l’équilibre général de
+            notre organisme.
+          </p>
+        </article>
+      </div>
     </section>
   </main>
 
