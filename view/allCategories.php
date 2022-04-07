b8cf0617d22b66b6b108945f7cc116a7a82974b5
TTHELLIER <t.thellier@gmail.com>
2022-04-07 11:34:21 +0200

    html css allCategorie en cours

---
+++ view/allCategories.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/style.css">
+    <title>Catégories</title>
+</head>
+<body>
+    
+    <div class=category>
+        <div class=container>
+            <div id=boisson>
+
+            </div>
+        
+            <div class=description>
+                <h3>Les Boissons</h3>
+
+                <p>
+                Plus qu’indispensable, l’eau est vitale. Notre corps est constitué d’environ 60 à 70 % d’eau. 
+                Les pertes journalières peuvent entraîner une déshydratation qui implique rapidement des troubles fonctionnels de l’organisme. 
+                Il ne faut donc pas attendre d’avoir soif pour boire.
+                </p>
+            </div>
+
+        </div>
+    </div>
+
+    <div class=category>
+        <div class=container>
+        
+            <div class=description>
+                <h3>Fruits & Légumes</h3>
+
+                <p>
+                    Ce groupe d’aliments constitue un rôle fonctionnel et peu énergétique. Les fruits (et a minima les légumes) apportent du fructose. Ce sucre simple donne la saveur sucrée à ces aliments et équivaut énergétiquement au glucose.
+                </p>
+            </div>
+            <div id=fruit>
+
+            </div>
+
+        </div>
+    </div>
+
+    <div class=category>
+        <div class=container>
+            <div id=feculent>
+
+            </div>
+        
+            <div class=description>
+                <h3>Féculents</h3>
+
+                <p>
+                    Cette grande famille regroupe les pommes de terre, les céréales, le pain et les légumes secs... Elle est notre pre- mière source d’énergie et doit représenter la moitié de notre ration alimentaire quotidienne.
+                </p>
+            </div>
+
+        </div>
+    </div>
+
+    <div class=category>
+        <div class=container>
+        
+            <div class=description>
+                <h3>Produits Laitiers</h3>
+
+                <p>
+                    Ces aliments « bâtisseurs » ont un rôle très important pendant l’adolescence puisqu’ils participent au développement de la masse osseuse qui peut doubler pendant la puberté. Les produits laitiers sont nutritionnellement les aliments qui apportent la plus grande diversité d’éléments.
+                </p>
+            </div>
+
+            <div id=laitier></div>
+
+        </div>
+    </div>
+
+    <div class=category>
+        <div class=container>
+            <div id=viande>
+
+            </div>
+        
+            <div class=description>
+                <h3>Viande & Poisson & Oeuf & Légumineuse</h3>
+
+                <p>
+                    Ces aliments sont principalement recommandés pour leur richesse en protéines. La quantité de protéines apportée par la viande, le poisson ou les œufs est similaire. Par contre leurs teneurs en lipides, vitamines et minéraux sont très variables et sont fonction de l’animal et des morceaux cuisinés. C’est pourquoi nous conseillons de varier au maximum l’origine de ces aliments.
+                </p>
+            </div>
+
+        </div>
+    </div>
+
+    <div class=category>
+        <div class=container>
+        
+            <div class=description>
+                <h3>Matières Grasses</h3>
+
+                <p>
+                    Les matières grasses regroupent le beurre, la crème fraîche, les huiles et les margarines.
+                </p>
+            </div>
+
+            <div id=gras></div>
+        </div>
+    </div>
+
+    <div class=category>
+        <div class=container>
+            <div id=sucrerie>
+
+            </div>
+        
+            <div class=description>
+                <h3>Sucreries</h3>
+
+                <p>
+                    Ils regroupent tous les aliments ayant un goût sucré prononcé : chocolat, miel, confiture, viennoiserie, pâte à tartiner... cependant ces aliments apportent également des matières grasses dites « cachées ».
+                    La consommation de ces produits sucrés doit être modérée car une consommation importante de ceux-ci engendre un déséquilibre alimentaire. Les sportifs trouveront un intérêt dans ces aliments lors d’un effort prolongé.
+                </p>
+            </div>
+
+        </div>
+    </div>
+
+    
+</body>
+</html>
