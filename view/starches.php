8200862ec430f38a8d9f15c262ff66937d9bd936
TTHELLIER <t.thellier@gmail.com>
2022-04-08 14:34:22 +0200

    sugar.php completed, CSS in progress

---
+++ view/starches.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/style.css">
+    <title>Féculents</title>
+</head>
+<body>
+
+    <?php
+        require_once "../model/Header.php"
+    ?>
+    <div class=contenu>
+        <h2>Féculents</h2>
+
+        <div class=container>
+            <div id=img_bread></div>
+            <div class=descriptioncat>
+            <p>Cette grande famille regroupe les pommes de terre, les céréales, le pain et les légumes secs... Elle est notre première source d’énergie et doit représenter la moitié de notre ration alimentaire quotidienne.
+                Le groupe des féculents apporte les éléments suivants :</p>
+            </div>
+        </div>
+
+        <div class=container>
+
+            <div class=descriptioncat>
+                <h5>Fibres</h5>
+                <p>Présentes principalement dans les céréales complètes et les produits non raffinés, les fibres participent au bon confort digestif (régulation du transit intestinal, effet de satiété...).</p>
+                <h5>Nutriments</h5>
+                <p>- Glucides complexes : ramifié sous forme d’amidon, le glucose provenant de la dégradation de celui-ci est
+                    la première source d’énergie pour notre organisme. 
+                    Le glucose provenant de la digestion de l’amidon est distribué progressivement dans l’organisme et permet 
+                    d’assurer un état de satiété et des activités physiques et fonctions mentales à long terme.</p>
+            </div>
+
+                <div id=img_potatoes></div>
+        </div>
+
+        <div class=container>
+
+            <div id=img_rice></div>
+
+            <div class=descriptioncat>
+
+            <h5>Vitamines</h5>
+            <p>- Magnésium : équilibre le système nerveux, régularise le rythme cardiaque...</p>
+            <p>- Vitamine E : antioxydante (protection des tissus).</p>
+            <p>- Vitamines du groupe B : elles interviennent dans l’utilisation des nutriments.
+            Les teneurs en vitamines et minéraux sont très variables selon la céréale et le raffinage.</p>
+            </div>
+
+        </div>
+
+    </div>
+    
+</body>
+</html>
---
+++ view/sugar.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/style.css">
+    <title>Sucrerie</title>
+</head>
+<body>
+
+    <?php
+        require_once "../model/Header.php"
+    ?>
+    <div class=contenu>
+        <h2>Sucrerie</h2>
+
+        <div class=container>
+            <div id=img_cup></div>
+            <div class=descriptioncat>
+            <p>Ils regroupent tous les aliments ayant un goût sucré prononcé : chocolat, miel, confiture, viennoiserie, pâte à tartiner... cependant ces aliments apportent également des matières grasses dites « cachées ».</p>
+            <p>La consommation de ces produits sucrés doit être modérée car une consommation importante de ceux-ci engendre un déséquilibre alimentaire. Les sportifs trouveront un intérêt dans ces aliments lors d’un effort prolongé.</p>
+            </div>
+        </div>
+
+        <div class=container>
+
+            <div class=descriptioncat>
+                <h5>Nutriments</h5>
+                <p>- Glucides simples : ils constituent une source d’énergie en nous fournissant du glucose, combustible de toutes les cellules de notre corps. Ainsi, pour le cerveau, le glucose est la seule source d’énergie utilisable (il ne sait pas fabriquer des énergies de secours).</p>
+                <p>Les glucides simples sont rapidement digérés et augmentent rapidement les taux de sucre dans le sang. Ce groupe d’aliments est principalement source de PLAISIR...</p>
+            </div>
+
+                <div id=img_bonbon></div>
+        </div>
+
+    </div>
+    
+</body>
+</html>
