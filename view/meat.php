ddc00aa2207349c51f48763e4b0be5f5991d6665
sylvainflt <sylvainfoucault1@gmail.com>
2022-04-08 11:12:51 +0200

    page meat effectuée

--- view/meat.php
+++ view/meat.php
@@ -15,35 +15,58 @@
     include 'header.php';
     ?>
     <h2>Viandes, Poissons, Oeufs & Légumineuses</h2>
+    <main>
+        <div class=descriptioncat>
+            <div id=img_meat></div>
+            <p class="proteinesBlocs">
+                Ces aliments sont principalement recommandés pour
+                <span class="texteOrange">leur richesse en protéines</span>.
+                La quantité de protéines apportée par la viande, le poisson ou
+                les œufs est similaire. Par contre leurs teneurs en lipides,
+                vitamines et minéraux sont très variables et sont fonction
+                de l’animal et des morceaux cuisinés. C’est pourquoi nous
+                conseillons de <span class="texteOrange">varier</span> au
+                maximum <span class="texteOrange">l'origine</span> de ces
+                aliments.
+            </p>
+        </div>
 
-    <div class=descriptioncat>
-        <div id=img_meat></div>
-        <p>Ces aliments sont principalement recommandés pour leur richesse en protéines. La quantité de protéines apportée par la viande, le poisson ou les œufs est similaire. Par contre leurs teneurs en lipides, vitamines et minéraux sont très variables et sont fonction de l’animal et des morceaux cuisinés. C’est pourquoi nous conseillons de varier au maximum l’origine de ces aliments.</p>
-    </div>
+        <div class=descriptioncat>
+            <div class="proteinesBlocs">
+                <h5>Nutriments</h5>
+                <p>
+                    - <span class="texteOrange">Protéines</span> : elles sont composées d’acides aminés qui
+                    permettent la construction et le renouvellement des organes
+                    vitaux et des tissus (particulièrement les muscles).
+                    - <span class="texteOrange">Lipides</span> (matières grasses) : apportent de l’énergie
+                    mais leurs quantités et qualités sont très variables
+                    selon l’origine de
+                    l’animal.
+                </p>
+            </div>
+            <div id=img_fish></div>
+        </div>
 
-    <div class=descriptioncat>
-        <div id=img_fish></div>
-        <h5>Nutriments</h5>
-        <p>- Protéines : elles sont composées d’acides aminés qui permettent la construction et le renouvellement des organes vitaux et des tissus (particulièrement les muscles).
-            - Lipides (matières grasses) : apportent de l’énergie mais leurs quantités et qualités sont très variables selon l’origine de l’animal.</p>
-    </div>
+        <div class=descriptioncat>
+            <div id=img_eggs></div>
+            <div class="proteinesBlocs">
+                <h5>Minéraux</h5>
+                <p>- <span class="texteOrange">Fer</span> : fabrication d’éléments (hémoglobine et myoglobine) qui permettent le transport et le stockage de l’oxygène. Le fer d’origine animale est plus assimilable (x 2,5) par l’organisme que le fer d’origine végétale.
+                    - <span class="texteOrange">Zinc</span> : renforce le fonctionnement du système immunitaire et rôle antioxydant (protection contre le vieillissement).
+                    - <span class="texteOrange">Sélénium</span> : rôle antioxydant (protection contre le vieillissement)</p>
+            </div>
+        </div>
 
-    <div class=descriptioncat>
-        <div id=img_eggs></div>
-        <h5>Minéraux</h5>
-        <p>- Fer : fabrication d’éléments (hémoglobine et myoglobine) qui permettent le transport et le stockage de l’oxygène. Le fer d’origine animale est plus assimilable (x 2,5) par l’organisme que le fer d’origine végétale.
-            - Zinc : renforce le fonctionnement du système immunitaire et rôle antioxydant (protection contre le vieillissement).
-            - Sélénium : rôle antioxydant (protection contre le vieillissement)</p>
-    </div>
-
-    <div class=descriptioncat>
-        <div id=img_legume></div>
-        <h5>Vitamines</h5>
-        <p>- Vitamines du groupe B : elles sont multiples et leurs richesses dépendent de la quantité de matières grasses.
-            - Vitamine E : antioxydante (protection des tissus).
-            - Vitamine K : intervient dans la coagulation du sang.</p>
-    </div>
-
+        <div class=descriptioncat>
+            <div class="proteinesBlocs">
+                <h5>Vitamines</h5>
+                <p>- <span class="texteOrange">Vitamines du groupe B</span> : elles sont multiples et leurs richesses dépendent de la quantité de matières grasses.
+                    - <span class="texteOrange">Vitamine E</span> : antioxydante (protection des tissus).
+                    - <span class="texteOrange">Vitamine K</span> : intervient dans la coagulation du sang.</p>
+            </div>
+            <div id=img_legume></div>
+        </div>
+    </main>
 
 </body>
 
