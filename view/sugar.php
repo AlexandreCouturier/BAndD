ead490599ab97f5e43f2892b468fba5afcfc6e2c
TTHELLIER <t.thellier@gmail.com>
2022-04-10 11:29:36 +0200

    suger.php completed, css ok

--- view/sugar.php
+++ view/sugar.php
@@ -18,8 +18,8 @@
         <div class=container>
             <div id=img_cup></div>
             <div class=descriptioncat>
-            <p>Ils regroupent tous les aliments ayant un goût sucré prononcé : chocolat, miel, confiture, viennoiserie, pâte à tartiner... cependant ces aliments apportent également des matières grasses dites « cachées ».</p>
-            <p>La consommation de ces produits sucrés doit être modérée car une consommation importante de ceux-ci engendre un déséquilibre alimentaire. Les sportifs trouveront un intérêt dans ces aliments lors d’un effort prolongé.</p>
+            <p>Ils regroupent tous les aliments ayant <span> un goût sucré prononcé</span> : chocolat, miel, confiture, viennoiserie, pâte à tartiner... cependant ces aliments apportent également <span>des matières grasses</span> dites « <span>cachées</span> ».</p>
+            <p><span>La consommation</span> de ces produits sucrés <span>doit être modérée</span> car une consommation importante de ceux-ci <span>engendre un déséquilibre alimentaire</span>. Les sportifs trouveront un intérêt dans ces aliments lors d’un effort prolongé.</p>
             </div>
         </div>
 
@@ -27,8 +27,8 @@
 
             <div class=descriptioncat>
                 <h5>Nutriments</h5>
-                <p>- Glucides simples : ils constituent une source d’énergie en nous fournissant du glucose, combustible de toutes les cellules de notre corps. Ainsi, pour le cerveau, le glucose est la seule source d’énergie utilisable (il ne sait pas fabriquer des énergies de secours).</p>
-                <p>Les glucides simples sont rapidement digérés et augmentent rapidement les taux de sucre dans le sang. Ce groupe d’aliments est principalement source de PLAISIR...</p>
+                <p>- <span>Glucides simples</span> : ils constituent une <span>source d’énergie</span> en nous fournissant du glucose, <span>combustible de toutes les cellules</span> de notre corps. Ainsi, pour le cerveau, le glucose est la seule source d’énergie utilisable (il ne sait pas fabriquer des énergies de secours).</p>
+                <p><span>Les glucides simples</span> sont <span>rapidement</span> digérés et <span>augmentent</span> rapidement <span>les taux de sucre </span>dans le sang. Ce groupe d’aliments est principalement source de <span>PLAISIR</span>...</p>
             </div>
 
                 <div id=img_bonbon></div>
